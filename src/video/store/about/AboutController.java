ecd9d703949d236653481daba469cf5fe987e5e6
Amila Hrustić <ahrustic2@etf.unsa.ba>
2019-02-02 18:39:05 +0100

    dodan fxml, controller i main za about

---
+++ src/video/store/about/AboutController.java
@@ -0,0 +1,4 @@
+package video.store.about;
+
+public class AboutController {
+}
---
+++ src/video/store/about/AboutMain.java
@@ -0,0 +1,29 @@
+package video.store.about;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import video.store.login.LoginController;
+
+
+public class AboutMain extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("about.fxml"));
+        AboutController ctrl = new AboutController();
+        loader.setController(ctrl);
+        Parent root = loader.load();
+        primaryStage.setTitle("About");
+        primaryStage.setScene(new Scene(root, 260, 320));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
