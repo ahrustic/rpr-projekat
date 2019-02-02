ee671334e35ef1b03217daf6be56dabbf6c796f5
Amila Hrustić <ahrustic2@etf.unsa.ba>
2019-02-02 02:26:18 +0100

    napravljen fxml, kontroler i main za promjenu zaduzenja

---
+++ src/video/store/changessue/PromijeniZaduzenjeController.java
@@ -0,0 +1,4 @@
+package video.store.changessue;
+
+public class PromijeniZaduzenjeController {
+}
---
+++ src/video/store/changessue/PromijeniZaduzenjeMain.java
@@ -0,0 +1,28 @@
+package video.store.changessue;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import video.store.login.LoginController;
+
+
+public class PromijeniZaduzenjeMain extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("promijeniZaduzenje.fxml"));
+        PromijeniZaduzenjeController ctrl = new PromijeniZaduzenjeController();
+        loader.setController(ctrl);
+        Parent root = loader.load();
+        primaryStage.setTitle("Promijeni zaduzenje");
+        primaryStage.setScene(new Scene(root, 400, 400));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
