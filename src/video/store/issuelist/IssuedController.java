217b5c468aabf2181e6922dee458008af04d7877
Amila Hrustić <ahrustic2@etf.unsa.ba>
2019-02-02 02:28:22 +0100

    napravljen fxml, kontroler i main za listu zaduzenja

---
+++ src/video/store/issuelist/IssuedController.java
@@ -0,0 +1,4 @@
+package video.store.issuelist;
+
+public class IssuedController {
+}
---
+++ src/video/store/issuelist/IssuedMain.java
@@ -0,0 +1,28 @@
+package video.store.issuelist;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import video.store.login.LoginController;
+
+
+public class IssuedMain extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("issued.fxml"));
+        IssuedController ctrl = new IssuedController();
+        loader.setController(ctrl);
+        Parent root = loader.load();
+        primaryStage.setTitle("Issued");
+        primaryStage.setScene(new Scene(root, 722, 456));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
