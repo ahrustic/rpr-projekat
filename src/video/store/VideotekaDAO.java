59949d3e79a0ef68d8364a0826bfcc42d3dbb973
Amila Hrustić <ahrustic2@etf.unsa.ba>
2019-02-03 03:56:06 +0100

    zavrseni upiti

--- src/video/store/VideotekaDAO.java
+++ src/video/store/VideotekaDAO.java
@@ -15,8 +15,10 @@ public class VideotekaDAO {
     public static VideotekaDAO instance = null;
     public Connection connection;
 
-    private PreparedStatement getFilmoviUpit, dajFilmUpit, dajClanUpit, dajZaduzenjeUpit, dajFilmPoIDUpit, dajClanPoIDUpit, getClanoviUpit, getZaduzenjaUpit,
-                              obrisiFilmUpit, obrisiClanUpit, obrisiZaduzenjeUpit;
+    private PreparedStatement getFilmoviUpit, dajFilmUpit, dajClanUpit, dajZaduzenjeUpit, getClanoviUpit, getZaduzenjaUpit,
+                              obrisiFilmUpit, obrisiClanUpit, obrisiZaduzenjeUpit, dodajClanUpit, dodajFilmUpit, dodajZaduzenjeUpit, odrediIdClanaUpit,
+                              odrediIdFilmaUpit, odrediIdZaduzenjaUpit, nadjiClanaUpit, nadjiFilmUpit, nadjiZaduzenjeUpit, promijeniClanUpit,
+                              promijeniFilmUpit, promijeniZaduzenjeUpit;
 
     public  static VideotekaDAO getInstance(){
         if (instance == null) instance = new VideotekaDAO();
@@ -67,6 +69,23 @@ public class VideotekaDAO {
             obrisiClanUpit = connection.prepareStatement("DELETE FROM clan WHERE id=?");
             obrisiFilmUpit = connection.prepareStatement("DELETE FROM film WHERE id=?");
             obrisiZaduzenjeUpit = connection.prepareStatement("DELETE FROM zaduzenje WHERE id=?");
+
+            dodajClanUpit = connection.prepareStatement("INSERT INTO clan VALUES(?,?,?,?)");
+            dodajFilmUpit = connection.prepareStatement("INSERT INTO film VALUES(?,?,?,?,?,?)");
+            dodajZaduzenjeUpit = connection.prepareStatement("INSERT INTO zaduzenje VALUES(?,?,?,?,?)");
+
+            odrediIdClanaUpit = connection.prepareStatement("SELECT MAX(id)+1 FROM clan");
+            odrediIdFilmaUpit = connection.prepareStatement("SELECT MAX(id)+1 FROM film");
+            odrediIdZaduzenjaUpit = connection.prepareStatement("SELECT MAX(id)+1 FROM zaduzenje");
+
+            nadjiClanaUpit = connection.prepareStatement("SELECT * FROM clan WHERE naziv=?");
+            nadjiFilmUpit = connection.prepareStatement("SELECT * FROM film WHERE naziv=?");
+            nadjiZaduzenjeUpit = connection.prepareStatement("SELECT * FROM zaduzenje WHERE id=?");
+
+            promijeniClanUpit = connection.prepareStatement("UPDATE clan SET naziv=?, email=?, broj_telefona=? WHERE id=?");
+            promijeniFilmUpit = connection.prepareStatement("UPDATE film SET naziv=?, zanr=?, godina_izdanja=?, glavni_glumac=?, kolicina=? WHERE id=?");
+            promijeniZaduzenjeUpit = connection.prepareStatement("UPDATE zaduzenje SET film=?, clan=?, datum_zaduzenja=?, datum_razduzenja=? WHERE id=?");
+
 
         } catch (SQLException e) {
             //e.printStackTrace();
@@ -217,7 +236,151 @@ public class VideotekaDAO {
         }
     }
 
+    public void dodajClan(Clan clan) {
+        try {
+            ResultSet rs = odrediIdClanaUpit.executeQuery();
+            int id = 1;
+            if (rs.next()) {
+                id = rs.getInt(1);
+            }
 
+            dodajClanUpit.setInt(1, id);
+            dodajClanUpit.setString(2, clan.getNaziv());
+            dodajClanUpit.setString(3, clan.getEmail());
+            dodajClanUpit.setString(4, clan.getBrojTelefona());
+            dodajClanUpit.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void dodajFilm(Film film) {
+        try {
+            ResultSet rs = odrediIdFilmaUpit.executeQuery();
+            int id = 1;
+            if (rs.next()) {
+                id = rs.getInt(1);
+            }
+
+            dodajFilmUpit.setInt(1, id);
+            dodajFilmUpit.setString(2, film.getNaziv());
+            dodajFilmUpit.setString(3, film.getZanr());
+            dodajFilmUpit.setInt(4, film.getGodnaIzdanja());
+            dodajFilmUpit.setString(5, film.getGlavniGlumac());
+            dodajFilmUpit.setInt(6, film.getKolicina());
+
+            dodajFilmUpit.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void dodajZaduzenje(Zaduzenje zaduzenje) {
+        try {
+            ResultSet rs = odrediIdZaduzenjaUpit.executeQuery();
+            int id = 1;
+            if (rs.next()) {
+                id = rs.getInt(1);
+            }
+
+            dodajZaduzenjeUpit.setInt(1, id);
+            dodajZaduzenjeUpit.setInt(2, zaduzenje.getClan().getId());
+            dodajZaduzenjeUpit.setInt(3, zaduzenje.getFilm().getId());
+            dodajZaduzenjeUpit.setDate(4, Date.valueOf(zaduzenje.getDatumZaduzenja()));
+            dodajZaduzenjeUpit.setDate(4, Date.valueOf(zaduzenje.getDatumRazduzenja()));
+            dodajZaduzenjeUpit.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Clan nadjiClan(String nazivClana) {
+        try {
+            nadjiClanaUpit.setString(1, nazivClana);
+            ResultSet rs = nadjiClanaUpit.executeQuery();
+            if (!rs.next()) return null;
+            return dajClanIzResultSeta(rs);
+        } catch (SQLException e) {
+            //e.printStackTrace();
+            return null;
+        }
+    }
+
+    public Film nadjiFilm(String nazivFilma) {
+        try {
+            nadjiFilmUpit.setString(1, nazivFilma);
+            ResultSet rs = nadjiFilmUpit.executeQuery();
+            if (!rs.next()) return null;
+            return dajFilmIzResultSeta(rs);
+        } catch (SQLException e) {
+            //e.printStackTrace();
+            return null;
+        }
+    }
+
+    public Zaduzenje nadjiZaduzenje(String idZaduzenja) {
+        try {
+            nadjiZaduzenjeUpit.setString(1, idZaduzenja);
+            ResultSet rs = nadjiZaduzenjeUpit.executeQuery();
+            if (!rs.next()) return null;
+            return dajZaduzenjeIzResultSeta(rs);
+        } catch (SQLException e) {
+            //e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void promijeniClana(Clan clan) {
+        try {
+
+            promijeniClanUpit.setString(1, clan.getNaziv());
+            promijeniClanUpit.setString(2, clan.getEmail());
+            promijeniClanUpit.setString(3, clan.getBrojTelefona());
+            promijeniClanUpit.setInt(4, clan.getId());
+            promijeniClanUpit.executeUpdate();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void promijeniFilm(Film film) {
+        try {
+
+            promijeniFilmUpit.setString(1, film.getNaziv());
+            promijeniFilmUpit.setString(2, film.getZanr());
+            promijeniFilmUpit.setInt(3, film.getGodnaIzdanja());
+            promijeniFilmUpit.setString(4, film.getGlavniGlumac());
+            promijeniFilmUpit.setInt(5, film.getKolicina());
+            promijeniFilmUpit.setInt(6, film.getId());
+
+            promijeniFilmUpit.executeUpdate();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void promijeniZaduzenje(Zaduzenje zaduzenje) {
+        try {
+
+            promijeniZaduzenjeUpit.setInt(1, zaduzenje.getClan().getId());
+            promijeniZaduzenjeUpit.setInt(2, zaduzenje.getFilm().getId());
+            promijeniZaduzenjeUpit.setDate(3, Date.valueOf(zaduzenje.getDatumZaduzenja()));
+            promijeniZaduzenjeUpit.setDate(4, Date.valueOf(zaduzenje.getDatumRazduzenja()));
+            promijeniZaduzenjeUpit.setInt(5, zaduzenje.getId());
+            promijeniZaduzenjeUpit.executeUpdate();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
